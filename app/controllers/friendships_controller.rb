ba7c110bf73e48f138f97fe594e12503e2fbd351
Ubuntu <ubuntu@ip-172-31-20-66.us-east-2.compute.internal>
2020-07-10 10:53:13 +0000

    added accept and reject friendship invitations functionality.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,4 +3,17 @@ class FriendshipsController < ApplicationController
         @new_request = Friendship.create(user_id: params[:user_id], friend_id: params[:friend_id])
         redirect_to users_path
     end
+    
+    def update
+        @user = User.find(params[:id])
+        @friend = Friendship.find_by(user_id: params[:user_id], friend_id: params[:friend_id])
+        @friend.update(status: true)
+        redirect_to user_path
+    end
+    
+    def destroy
+          @friend = Friendship.find_by(user_id: params[:user_id], friend_id: params[:friend_id])
+          @friend.destroy
+          redirect_to user_path
+    end
 end
